62dd9de631fc229517c647b4556da6fbe87f9d36
Bityta <bituckihs@gmail.com>
2023-02-18 20:07:39 +0300

    Дороботка

--- tgBot.py
+++ tgBot.py
@@ -9,6 +9,7 @@ from datetime import date
 import sqlite3
 from random import randint
 
+
 global num
 num = 0
 
@@ -27,9 +28,11 @@ def run_bot():
     button1 = KeyboardButton('Знаю')
     button2 = KeyboardButton('Не знаю')
     button3 = KeyboardButton('Закончить')
-
+    button4 = KeyboardButton('Далее')
     keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(
         button1, button2).add(button3)
+    keyboard2 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(
+        button4).add(button3)
 
     @dp.message_handler(commands=["start"])
     async def command_start_handler(msg: types.Message):
@@ -46,41 +49,53 @@ def run_bot():
         dif = 3
         await msg.answer(f"{sql_word(num, msg.from_user.id,dif)}", reply_markup=keyboard1)
     # решить что то с num
-    #с dif решить
-    #сделать кнопку далее
+    # с dif решить
+    # сделать кнопку далее
 
     @dp.message_handler()
     async def word_handler(msg: types.Message):
         if msg.text == 'Знаю':
             conn = sqlite3.connect(f'UsersData\\{msg.from_user.id}.db')
             cur = conn.cursor()
-            # cur.execute(f"UPDATE word SET difficulty=difficulty-1 WHERE id == {num} and difficulty >0")
+            cur.execute(f"UPDATE word SET difficulty=difficulty-1 WHERE id == {num} and difficulty >0")
+            cur.execute(f"SELECT * FROM word WHERE difficulty == {dif}")
+
+            word = cur.fetchall()[num]
+            word_en = (word[1])
+            word_ru = (word[2])
+            word_dif = (word[3])
             conn.commit()
-            # num = randint(0, 499)
-            # await msg.answer(f"{sql_word(num, msg.from_user.id)}", reply_markup=keyboard1)
+            await  msg.answer(f"{word_en} - {word_ru}\nТекущая сложность слова = {word_dif}",  reply_markup=keyboard2)
+            
 
         elif msg.text == 'Не знаю':
-        	num,dif = 1,3
-        
-        	conn = sqlite3.connect(f'UsersData\\{msg.from_user.id}.db')
-        	cur = conn.cursor()
-	        cur.execute(f"UPDATE word SET difficulty=difficulty+1 WHERE id == {num} and difficulty <5")
-        	cur.execute(f"SELECT * FROM word WHERE difficulty == {dif}")
 
-        	word = cur.fetchall()[num]
-	        word_en = (word[1])
-	        word_ru = (word[2])
-	        word_dif = (word[3])
+            num, dif = 1, 3
 
-	        conn.commit()
+            conn = sqlite3.connect(f'UsersData\\{msg.from_user.id}.db')
+            cur = conn.cursor()
+            cur.execute(f"UPDATE word SET difficulty=difficulty+1 WHERE id == {num} and difficulty <5")
+            cur.execute(f"SELECT * FROM word WHERE difficulty == {dif}")
 
-	        await  msg.answer (f"{word_en} - {word_ru}\nТекущая сложность слова = {word_dif}")
-	        # num = randint(0, 499)
-	        # await msg.answer(f"{sql_word(num, msg.from_user.id)}", reply_markup=keyboard1)
+            word = cur.fetchall()[num]
+            word_en = (word[1])
+            word_ru = (word[2])
+            word_dif = (word[3])
 
-        elif msg.text == "Закончить" or "/stop" or "/end":
-            await msg.answer(f"Спасибо за тест", reply_markup=ReplyKeyboardRemove())
             conn.commit()
+
+            await  msg.answer(f"{word_en} - {word_ru}\nТекущая сложность слова = {word_dif}",  reply_markup=keyboard2)
+            
+        elif msg.text == "Далее":
+        	num = randint(0, 499)
+        	dif = 3
+        	await msg.answer(f"{sql_word(num, msg.from_user.id,dif)}", reply_markup=keyboard1)
+
+
+        elif (msg.text == "Закончить") or (msg.text ==  "/stop") or (msg.text ==  "/end"):
+            await msg.answer(f"Спасибо за тест", reply_markup=ReplyKeyboardRemove())
+
+        
     executor.start_polling(dp, skip_updates=True)
 
 
@@ -89,14 +104,11 @@ def sql_word(rand, num, dif=-1):
     conn = sqlite3.connect(f'UsersData\\{num}.db')
     cur = conn.cursor()
 
-    if dif >=1 and dif<= 5:
-    	cur.execute(f"SELECT * FROM word WHERE difficulty == {dif}")
-        
+    if dif >= 1 and dif <= 5:
+        cur.execute(f"SELECT * FROM word WHERE difficulty == {dif}")
 
     else:
-    	cur.execute(f"SELECT * FROM word")
-
-        
+        cur.execute(f"SELECT * FROM word")
 
     word = cur.fetchall()[rand][1]
     conn.commit()
